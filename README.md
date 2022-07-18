44ac5d31f5ff7d8215d62c7bd2d8ee6f1419bbe6
pozabil <partnet.biz@gmail.com>
2022-07-18 19:28:38 +0400

    sign_in feature added (v.0.0.9)

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ## QnA
 
-version 0.0.8
+version 0.0.9
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :user do
-    
+    email { 'test@user.test' }
+    password { 'correct_password1'}
+
+    trait :wrong do
+      password { 'wrong_password2'}
+    end
   end
 end
